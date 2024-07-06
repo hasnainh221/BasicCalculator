e1f66751dc1111d9309a66ddec3619b8a2ad4d26
Hasnain Haider <111046199+hasnainh221@users.noreply.github.com>
2024-07-06 06:38:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# basic calculator use of basic arithmetic operators
+a=10
+b=10
+ans1=a+b
+print("Addition of",a,"and",b,"is", ans1)
+ans2=a-b
+print("Subtraction of",a,"and",b,"is", ans2)
+ans3=a*b
+print("Multiplication of",a,"and",b,"is", ans3)
+ans4=a/b
+print("divion of",a,"and",b,"is", ans4)
+ans5=a%b
+print("modulus of",a,"and",b,"is", ans5)
+ans6=a//b
+print("floor division of",a,"and",b,"is", ans6)
+ans7=a**b
+print("exponential of",a,"and",b,"is", ans7)
+
+
